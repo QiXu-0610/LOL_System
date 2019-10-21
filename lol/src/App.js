ac7fbf1bb41ad95037878fdc198378ae659fa744
wsl <wsl@gmail.com>
2019-10-21 19:56:31 +0800

    'initless'

--- lol/src/App.js
+++ lol/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.less'
 
 function App() {
   return (
     <div className="App">
-       login1
-       login2
+       
     </div>
   );
 }
