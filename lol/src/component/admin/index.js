3452095554af219de251693d305626bae94d5c25
xulifang1127 <52695950+xulifang1127@users.noreply.github.com>
2019-10-21 22:02:46 +0800

    'bb1-补充'

--- lol/src/component/admin/index.js
+++ lol/src/component/admin/index.js
@@ -1,12 +1,18 @@
 import React,{Component} from 'react';
 import {Button} from 'antd';
-import './admin.css'
+import './admin.less'
 class Admin extends Component{ 
 render(){
     return(
         <div className='admin'>
-            首页
-            <Button>hhhhhh</Button>
+            <div className='admin-nav'>
+                导航
+            </div>
+            <div className='admin-content'>
+                <div>头部</div>
+                <div>子路由</div>      
+            </div>
+
         </div>
     )
 }
