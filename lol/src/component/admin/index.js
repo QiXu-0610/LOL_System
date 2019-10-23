444028719cc878fdc16111cc013869d73e1d0539
xulifang1127 <52695950+xulifang1127@users.noreply.github.com>
2019-10-23 23:01:02 +0800

    'load'

--- lol/src/component/admin/index.js
+++ lol/src/component/admin/index.js
@@ -13,9 +13,9 @@ render(){
             <CustomNav></CustomNav>
             </div> 
             <div className='admin-content'>
-                <div>头部</div>
+               
                 <div>{this.props.children}</div>
-                <div>底部</div>      
+            
             </div>
 
         </div>
