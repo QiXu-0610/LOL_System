9a816b1ef0066eaa9c46f56fe7ed5a3dd766cffe
Lululuuuuussr <52695285+Lululuuuuussr@users.noreply.github.com>
2019-10-24 15:40:07 +0800

    'add'

--- lol/src/component/user/userAdd.js
+++ lol/src/component/user/userAdd.js
@@ -1,8 +1,26 @@
 import React,{Component} from 'react';
+import {Card,Input} from 'antd'
+import './userAdd.less'
+
 class UserAdd extends Component{ 
 render(){
     return(
-        <div>这是在Add</div>
+        <div className='userAdd-box'>
+            <Card className="userAdd-content">
+                <h1>召唤师信息</h1>
+                <div className="userAdd-list">
+                  <input placeholder="召唤师"/>
+                  <input placeholder="段位"/>
+                  <input placeholder="等级"/>
+                  <input placeholder="胜率"/>
+                  <div>
+                     <input type="file" />
+                     <button>上传</button>
+                     <img src="" alt=""/>
+                  </div> 
+                  </div>    
+            </Card>
+        </div>
     )
  }
 }
--- lol/src/component/user/userList.js
+++ lol/src/component/user/userList.js
@@ -1,75 +1,148 @@
-import React,{Component} from 'react';
+// import React,{Component} from 'react';
+// import {Card,Table} from 'antd'
+
+//   const columns = [
+//     {
+//         title: '',
+//         dataIndex: 'index',
+//         key: 'index',
+//       },
+//     {
+//       title: '召唤师',
+//       dataIndex: 'name',
+//       key: 'name',
+//     },
+//     {
+//       title: '段位',
+//       dataIndex: 'rank',
+//       key: 'rank',
+//     },
+//     {
+//       title: '等级',
+//       dataIndex: 'grd',
+//       key: 'grd',
+//     },
+//     {
+//       title: '胜率',
+//       dataIndex: 'win',
+//       key: 'win',
+//     },
+//   ];
+ 
+// class UserList extends Component{ 
+//     constructor(){
+//         super()
+        
+//         this.state={
+//             list:[]
+//         }
+//     }
+    
+//     componentDidMount() {
+//         this.$axios.get('/py/lolzhs/getzhs')
+//         .then((data)=>{
+//             console.log(data.data.list)
+            
+//             this.setState({list:data.data.list})
+//             console.log(this.state.list)
+//          })
+        
+//     }
+//     render(){
+//         console.log(this.state.list)
+//         let {list} = this.state
+        
+//         return(
+//             <div>
+//                 <Card>
+//                     <Table 
+//                         columns={columns}
+//                         dataSource={list} 
+                    
+//                     />
+//                 </Card>
+//             </div>
+//         )
+//     }
+//     }
+//     export default UserList
+
+import React,{Component} from 'react'
 import {Card,Table} from 'antd'
 
-const dataSource = [
-    {
-      key: '1',
-      name: '胡彦斌',
-      age: 32,
-      address: '西湖区湖底公园1号',
-     
-    },
-    {
-      key: '2',
-      name: '胡彦祖',
-      age: 42,
-      address: '西湖区湖底公园1号',
-     
-    },
-  ];
-  
-  const columns = [
-    {
-      title: '召唤师',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: '段位',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: 'LP',
-      dataIndex: 'address',
-      key: 'address',
-    },
-    {
-      title: '等级',
-      dataIndex: 'address',
-      key: 'address',
-    },
-    {
-      title: '胜率',
-      dataIndex: 'address',
-      key: 'address',
-      render(){
-          return(
-            <span></span>
-          )
-      }
-    },
-  ];
-class UserList extends Component{ 
-componentDidMount(){
-    let url = '/py/lolhero/gethero'
-    this.$axios.get(url)
-    .then((data)=>{
-        console.log(data,111)
-    })
-    .catch(()=>{
-        console.log('err')
-    })
-}
-render(){
+
+var columns = [
+            {
+                title: '',
+                dataIndex: 'zhsimg',
+                key: 'py',
+                render(zhsimg){
+                    let url=`py${zhsimg}`
+                    
+                    return(
+                        <img src={url} alt=''/>
+                    )
+                }
+              },
+            {
+              title: '召唤师',
+              dataIndex: 'zhsname',
+              key: 'name',
+            },
+            {
+              title: '段位',
+              dataIndex: 'zhsrank',
+              key: 'zhsrank',
+            },
+            {
+              title: '等级',
+              dataIndex: 'zhsgrd',
+              key: 'zhsgrd',
+            },
+            {
+              title: '胜率',
+              dataIndex: 'zhswin',
+              key: 'zhswin',
+            },
     
-    return(
-        <div className='userlist-box'>
-            <Card>
-                <Table dataSource={dataSource} columns={columns}></Table>
-            </Card>
-        </div>
-    )
- }
+  ];
+  // const dataSource = [
+  //     {name:'网易',age:15,address:'老牛湾',sex:0,key:1,img:'456789'},
+  //     {name:'网易',age:15,address:'老牛湾',sex:1,key:2,img:'456789'},
+  //     {name:'网易',age:15,address:'老牛湾',sex:2,key:3,img:'456789'},
+  // ]
+class UserList extends  Component{
+    constructor(){
+        super()
+        this.state={
+            list:[]
+        }
+    }
+    componentDidMount(){
+       this.refreshData()
+    }
+    refreshData(){
+        let url = `/py/lolzhs/getzhs`
+        this.$axios.get(url)
+        .then((data)=>{
+          console.log(data)
+          this.setState({list:data.data.list})   
+        })
+        
+    }
+    render(){
+        let {list} = this.state
+        return(
+                        <div>
+                            <Card>
+                                <Table 
+                                    columns={columns}
+                                    dataSource={list} 
+                                
+                                />
+                            </Card>
+                        </div>
+                    )
+    }
 }
 export default UserList
