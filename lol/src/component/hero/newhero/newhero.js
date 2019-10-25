78a67b66a6fe7aaeb8bbd4d0a4e7f78980d0e35f
xulifang1127 <52695950+xulifang1127@users.noreply.github.com>
2019-10-25 14:41:57 +0800

    'newhero'

--- lol/src/component/hero/newhero/newhero.js
+++ lol/src/component/hero/newhero/newhero.js
@@ -1,9 +1,13 @@
-import React,{Component} from 'react';
-import { Card, Select ,Input,Upload,Button} from 'antd';
+import React,{Component,Fragment} from 'react';
+// import {WithRouter}  from react-router-dom
+import { Card, Select ,Input,Upload,Button,Icon,Modal,notification } from 'antd';
 
-// const { Option } = Select;
+import './newHero.less'
+
+ const { Option } = Select;
 const type_id=[1,2,3,4,5]
 const match ={1:'上单',2:'打野',3:'中单',4:'射手',5:'辅助'}
+
 
 class HeroNew extends Component{ 
  constructor(){
@@ -13,25 +17,60 @@ class HeroNew extends Component{
          name:'',
          info:'',
          type:'',
-         type_id:''
+         type_id:'',
+         visible:false
 
      }
  }
+ initData=()=>{
+    this.setState({
+    img:'',
+    name:'',
+    info:'',
+    type:'',
+    type_id:'',
+    visible:false})
+ }
+ onChange=(value)=> {
+    console.log(`selected ${value}`);
+    this.setState({type_id:value,type:match[value]})
 
+    
+  }
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
 
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+ openNotification = () => {
+    const args = {
+      message: '上传成功',
+      description:
+        'ok',
+      duration: 0.3,
+    };
+    notification.open(args);
+    setInterval(()=>{ this.props.history.push('/admin/hero/list/qb')},1000)
+   
+            
+  };
  upload=()=>{
     let file=this.refs.file.files[0]
     let formdata=new FormData()
     formdata.append('hehe',file)
-//     console.log(this.refs.file.files)
-//     console.log(formdata.get('hehe'))
-//     let config = {
-//       headers:{'Content-Type':'multipart/form-data'}
-//    };
     this.$axios.post('/py/lolfile/img',formdata)
     .then((data)=>{
-      console.log(data.data.imgPath)
-      this.setState({img:data.data.imgPath})
+ 
+      this.setState({img:data.data.imgPath,visible:true})
+
     })
     .catch((err)=>{
         console.log(err)
@@ -43,7 +82,13 @@ class HeroNew extends Component{
           this.$axios.get(`py/lolhero/addhero?img=${this.state.img}&name=${this.state.name}
           &type=${this.state.type}&info=${this.state.info}&type_id=${this.state.type_id}`,)
           .then((data)=>{
-              console.log(data)
+
+          console.log(data)
+          if(data.data.err ==0){
+            this.openNotification()
+
+             
+          }
           })
 
       }else{
@@ -53,40 +98,80 @@ class HeroNew extends Component{
 render(){
     return(
         <div className='newHero'>
-            <Card>
-           <h4>新英雄</h4>
+           
+            <Card   >
+        
+           <h2>新英雄</h2>
+
+           <div className='inp'>
             <label>英雄名称</label>
-            <Input type='text' value ={this.state.name} onChange={
+            <Input type='text' value ={this.state.name}
+            style={{ width: 240}}
+            onChange={
                 (e)=>{
                     this.setState({name:e.target.value})
                  }
                 }/>
+            </div>
+
+
             <br/>
+            <div className='inp'>
             <label>英雄描述</label>
-            <Input type='text' value ={this.state.info} onChange={
+            <Input type='text' value ={this.state.info} 
+            style={{ width: 240}}
+            onChange={
                 (e)=>{
                     this.setState({info:e.target.value})
                  }
                 }/>
+            </div>
             <br/>
+            <div className='inp'>
             <label>英雄类别</label>
-            <Input type='text' value ={this.state.type_id} onChange={
-            (e)=>{
-                this.setState({type_id:e.target.value,type:match[e.target.value]})
-                }
-            }/>
-      
+            <Select
+                showSearch
+                style={{ width: 200 }}
+                value ={this.state.type_id}
+                placeholder="dddd"
+                onChange={this.onChange }
+                >
+            {type_id.map((item,index)=>{
+                return(
+                    <Option value={item}>{item}</Option>
+                )
+            })}
+
         
-       
+
+            </Select>
+            </div>
             <br/>
+            <div className='inp'>
+            <label>类别名称</label>
+            <Input type='text' value ={this.state.type}  style={{ width: 240}}/>
+           </div>
+
+            <br/>
+            <div className='inp'>
             <input type='file' ref = 'file'></input>
             <Button onClick={this.upload}>上传</Button>
-        
+            <Modal
     
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+         
+        >
+          <p>图片上传成功</p>
+        
+        </Modal>
+            </div>
+            <br/>
             <Button onClick = {this.submit}>提交</Button>
-
+             
             </Card>
- 
+           
         </div>
     )
  }
