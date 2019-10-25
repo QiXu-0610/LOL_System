8183870c941ab664c9b482482058cb3dc3021c6b
wsl <wsl@gmail.com>
2019-10-25 20:39:46 +0800

    'zh'

--- lol/src/component/setting/qt.js
+++ lol/src/component/setting/qt.js
@@ -2,7 +2,9 @@ import React,{Component} from 'react';
 class Qt extends Component{ 
 render(){
     return(
-        <div>这是其他</div>
+        <div>
+            
+        </div>
     )
  }
 }
---
+++ lol/src/component/setting/setsuccess.js
@@ -0,0 +1,22 @@
+// import React,{Component} from 'react';
+// import {Button,Result} from 'antd'
+// import './set.less'
+// class Su extends Component{ 
+// render(){
+//     return(
+//         <div>
+//             <Result className='success'
+//                         status="success"
+//                         title="添加成功"
+//                         extra={[
+//                         // <Button type="primary" key="console" onClick={this.sx.bind(this)}> 
+                            
+//                         // </Button>
+                       
+//                         ]}
+//             />
+//         </div>
+//     )
+//  }
+// }
+// export default Su
